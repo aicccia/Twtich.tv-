e2347794f476ee77d1cd89f66f2424d7671f470d
Nicholas Engleman <nicholas@englemaninvest.com>
2016-07-22 21:05:28 +0800

    1,02

--- twitch.js
+++ twitch.js
@@ -16,37 +16,44 @@ $(document).ready(function() {
             "Test_channel",
             "thomasballinger",
             "noobs2ninjas"];
+
     var url = "https://api.twitch.tv/kraken/streams/";
-    var game;
-    console.log(users.length);
+    var game, preview;
 
     for(var i=0;i<15;i++) {
-        var account = users[i];
-        $.getJSON(url + users[i], function(status) {
-           // console.log(status.stream._id);
 
-            if (!status.stream) {
-                game = "offline";
-                $(".streamer").after("" +
-                    "<div class=\"streamer\">" +
-                        "<div class=\"profilePic\"></div>" +
-                         "<div class=\"profileName\"><p>" + account + "</p></div>" +
-                        "<div class=\"profileStatus\"><p>" + game + "</p></div>" +
-                    "</div>");
+        (function () {
+            var account = users[i];
+            $.getJSON(url + users[i], function (status) {
+                // console.log(status.stream._id);
+
+
+                if (!status.stream) {
+                    game = "offline";
+                    $(".seperator").after("" +
+                        "<div class=\"streamer offline\">" +
+                            "<div class=\"profilePic\"></div>" +
+                            "<div class=\"profileName\"><p>" + account + "</p></div>" +
+                            "<div class=\"profileStatus\"><p>" + game + "</p></div>" +
+                        "</div>");
                 }
 
 
-            if (status.stream) {
-                game = status.stream.game;
-                $(".streamer").after("" +
-                    "<div class=\"streamer\">" +
-                        "<div class=\"profilePic\"></div>" +
-                        "<div class=\"profileName\"><p>" + account + "</p></div>" +
-                        "<div class=\"profileStatus\"><p>" + game + "</p></div>" +
-                    "</div>");
+                if (status.stream) {
+                    game = status.stream.game;
+                    var pic = status.stream._links.preview.small;
+                    console.log(pic);
+
+                    $("header").after("" +
+                        "<div class=\"streamer online\">" +
+                            "<div class=\"profilePic\"><img src=pic></div>" +
+                            "<div class=\"profileName\"><p>" + account + "</p></div>" +
+                            "<div class=\"profileStatus\"><p>" + game + "</p></div>" +
+                        "</div>");
                 }
 
             });
+        })();
         }
 });
 
